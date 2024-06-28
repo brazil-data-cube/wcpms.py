c586991d20e0b50f1a8abe04cbb52e96e9677140
GSansigolo <gabrielsansigolo@gmail.com>
2024-06-28 10:38:09 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,12 @@ setup(
     author_email = "gabrielsansigolo@gmail.com",
     url = "https://github.com/GSansigolo/wcpms.py",
     install_requires= [
+        "urllib3==2.2.2",
+        "requests==2.32.3",
+        "pandas==2.2.2",
+        "matplotlib==3.9.0",
+        "scipy==1.13.1",
+        "datetime==5.5"
     ],
     long_description = LONG_DESCRIPTION,
     setup_requires=['pytest-runner'],
